2d90981056b14272ab9eeefbc8b8e719c22f565e
Irfan wani <irfanwani347@gmail.com>
2021-05-21 11:12:21 +0530

    added the authentication-backend

---
+++ leadmanager/accounts/api.py
@@ -0,0 +1,43 @@
+from rest_framework import generics, permissions, serializers
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import LoginSerializer, UserSerializer, RegisterSerializer
+
+
+# Register API
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = AuthToken.objects.create(user)
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': token[1]
+        })
+
+# Login API
+class LoginApi(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': AuthToken.objects.create(user)[1]
+        })
+
+# Get User API
+class UserApi(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
